c0dd60ad6e97f218f8d9666bcca98351bddeca5c
ito <ittooo66@gmail.com>
2020-10-11 01:27:34 +0900

    delete curl path
    curlがデフォルトで使えたので変更

--- myAHKComponents/Resources/Scripts/PhilipsHueOff.bat
+++ myAHKComponents/Resources/Scripts/PhilipsHueOff.bat
@@ -1,3 +1,3 @@
-C:\"Program Files"\curl\src\curl.exe -X PUT http://192.168.10.20/api/wYcnCswCImWhoyfzFtGKhVgsS-W8H6J1S1LjcVbq/lights/1/state -d "{\"on\":false}"
-C:\"Program Files"\curl\src\curl.exe -X PUT http://192.168.10.20/api/wYcnCswCImWhoyfzFtGKhVgsS-W8H6J1S1LjcVbq/lights/2/state -d "{\"on\":false}"
-C:\"Program Files"\curl\src\curl.exe -X PUT http://192.168.10.20/api/wYcnCswCImWhoyfzFtGKhVgsS-W8H6J1S1LjcVbq/lights/3/state -d "{\"on\":false}"
+curl -X PUT http://192.168.10.20/api/wYcnCswCImWhoyfzFtGKhVgsS-W8H6J1S1LjcVbq/lights/1/state -d "{\"on\":false}"
+curl -X PUT http://192.168.10.20/api/wYcnCswCImWhoyfzFtGKhVgsS-W8H6J1S1LjcVbq/lights/2/state -d "{\"on\":false}"
+curl -X PUT http://192.168.10.20/api/wYcnCswCImWhoyfzFtGKhVgsS-W8H6J1S1LjcVbq/lights/3/state -d "{\"on\":false}"
--- myAHKComponents/Resources/Scripts/PhilipsHueOn.bat
+++ myAHKComponents/Resources/Scripts/PhilipsHueOn.bat
@@ -1,10 +1,9 @@
 set /a bri=254
-set curl="C:\Program Files\curl\src\curl.exe"
 
 if not "%1" == "" (
 	set bri=%1
 )
 
-%curl% -X PUT http://192.168.10.20/api/wYcnCswCImWhoyfzFtGKhVgsS-W8H6J1S1LjcVbq/lights/1/state -d "{\"on\":true,\"bri\":%bri%}"
-%curl% -X PUT http://192.168.10.20/api/wYcnCswCImWhoyfzFtGKhVgsS-W8H6J1S1LjcVbq/lights/2/state -d "{\"on\":true,\"bri\":%bri%}"
-%curl% -X PUT http://192.168.10.20/api/wYcnCswCImWhoyfzFtGKhVgsS-W8H6J1S1LjcVbq/lights/3/state -d "{\"on\":true,\"bri\":%bri%}"
+curl -X PUT http://192.168.10.20/api/wYcnCswCImWhoyfzFtGKhVgsS-W8H6J1S1LjcVbq/lights/1/state -d "{\"on\":true,\"bri\":%bri%}"
+curl -X PUT http://192.168.10.20/api/wYcnCswCImWhoyfzFtGKhVgsS-W8H6J1S1LjcVbq/lights/2/state -d "{\"on\":true,\"bri\":%bri%}"
+curl -X PUT http://192.168.10.20/api/wYcnCswCImWhoyfzFtGKhVgsS-W8H6J1S1LjcVbq/lights/3/state -d "{\"on\":true,\"bri\":%bri%}"
