8e6446c064be6c541029a3062a23e6bc854788c5
ittooo66 <ittooo66@gmail.com>
2022-12-02 09:30:01 +0900

    フォルダ構成の変更

--- myAHKComponents/Resources/Scripts/youtube-dl-mp3.ps1
+++ myAHKComponents/Resources/Scripts/youtube-dl-mp3.ps1
@@ -1,4 +1,4 @@
-$MyPath = Write-Output C:\Users\ittooo66\OneDrive\home\src\linux\YoutubeServer\urls\$((get-date).toString("yyyyMMddHHmmss"))
+$MyPath = Write-Output C:\Users\ittooo66\OneDrive\home\src\linux\YoutubeServer\urls\MusicDL\$((get-date).toString("yyyyMMddHHmmss"))
 $MyFile = Get-Clipboard -Format Text
 $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
 [System.IO.File]::WriteAllLines($MyPath, $MyFile, $Utf8NoBomEncoding)
