d709ca0a86ab614cb2710b4cb0bfd561be9d9af6
ittooo66 <ittooo66@gmail.com>
2023-05-27 19:48:21 +0900

    wslに依存しない処理に変更

--- myAHKComponents/Resources/Scripts/SetAudioDevice.ps1
+++ myAHKComponents/Resources/Scripts/SetAudioDevice.ps1
@@ -1,11 +1,11 @@
 # Need https://github.com/frgnca/AudioDeviceCmdlets
 
 #デバイス名を取得
-$device_name=Get-Content C:\Users\ittooo66\OneDrive\home\src\ahk\Env\AUDIO_DEVICE.txt -Raw
+$device_name=Get-Content C:\Users\ittoo\OneDrive\home\src\ahk\Env\AUDIO_DEVICE.txt -Raw
 
 #該当デバイス名を持つデバイスIDを取得し、ID指定でデバイス変更
 for($i=1; $i -le 10; $i++) {
-  if ( (Get-AudioDevice -Index $i | wsl grep $device_name | wsl wc -l ) -eq 1 ){
+  if ((Get-AudioDevice -Index $i).Name -like "*$device_name*" ){
     Set-AudioDevice -Index $i
     break
   }
