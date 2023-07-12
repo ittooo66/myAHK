dbdace78fa9a740ec99161319ea60ec29e16e61c
ittooo66 <ittooo66@gmail.com>
2023-07-13 00:31:41 +0900

    改行が入ってしまってURL回収ができないバグを修正

--- myAHKComponents/Resources/Scripts/youtube-dl-mp3.ps1
+++ myAHKComponents/Resources/Scripts/youtube-dl-mp3.ps1
@@ -1,4 +1,4 @@
 $MyPath = Write-Output C:\Users\ittoo\OneDrive\home\src\linux\YoutubeServer\urls\_ManualDL\$((get-date).toString("yyyyMMddHHmmss"))
 $MyFile = Get-Clipboard -Format Text
 $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
-[System.IO.File]::WriteAllLines($MyPath, $MyFile, $Utf8NoBomEncoding)
+[System.IO.File]::WriteAllText($MyPath, $MyFile, $Utf8NoBomEncoding)
