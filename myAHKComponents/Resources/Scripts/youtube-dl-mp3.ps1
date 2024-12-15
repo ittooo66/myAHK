17ea481f45d982e9314e25d4570f7ba4b88b3e9a
ittooo66 <ittooo66@gmail.com>
2024-12-16 05:14:56 +0900

    SRCパス変更

--- myAHKComponents/Resources/Scripts/youtube-dl-mp3.ps1
+++ myAHKComponents/Resources/Scripts/youtube-dl-mp3.ps1
@@ -1,4 +1,4 @@
-$MyPath = Write-Output C:\Users\ittoo\OneDrive\home\src\linux\YoutubeServer\urls\_ManualDL\$((get-date).toString("yyyyMMddHHmmss"))
+$MyPath = Write-Output D:\SRC\linux\RHEL9-Youtube\urls\_ManualDL\$((get-date).toString("yyyyMMddHHmmss"))
 $MyFile = Get-Clipboard -Format Text
 $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
 [System.IO.File]::WriteAllText($MyPath, $MyFile, $Utf8NoBomEncoding)
